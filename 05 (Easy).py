fffb899e04c397c1ba18293f68b8fa1c7bb7f4dc
Marcos Willian <markusuuuu@gmail.com>
2025-06-05 23:39:59 -0300

    wip: new challenge

--- 05 (Easy).py
+++ 05 (Easy).py
@@ -1,8 +1,35 @@
-from typing import List
+# Concatenation of Array
+# https://leetcode.com/problems/concatenation-of-array
+
+# You are given an integer array nums of length n. 
+# Create an array ans of length 2n where:
+
+# ans[i] == nums[i] and 
+# ans[i + n] == nums[i] for 
+# 0 <= i < n (0-indexed).
+
+# Example 1:
+# Input: nums = [1,4,1,2]
+# Output: [1,4,1,2,1,4,1,2]
+
+nums = [1,4,1,2]
+
+def getConcatenation(nums):
+    ans = [None] * (len(nums) * 2)
+
+    # def concatenateNext():
+    #     for j in range(0, len(ans)):
+    #         if ans[j] is None:
+    #             ans[j] = nums[j] 
+
+    for i in range(0, len(nums)):
+        # if i == len(nums):
+        #     concatenateNext()
+        #     break
+        ans[i] = nums[i]
+    
+    print(ans)
 
 
-# Input: nums = [3,2,2,3], val = 3
-# Output: 2, nums = [2,2,_,_]
-
-def removeElement(nums: List[int], val: int) -> int:
+getConcatenation(nums)
   
